9588a6f26424841dd6f420b9719f852e7ede7af6
Yupeng Ren <yupengren@Yupengs-MBP.hitronhub.home>
2018-03-15 22:51:54 -0500

    Login page and register page

---
+++ resources/views/register.blade.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<head>
+<meta charset="utf-8">
+    <title>Register</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <link href="{{ asset('css/demo.css') }}" rel="stylesheet" type="text/css">\
+</head>
+<body>
+<div class="flex-center position-ref full-height">
+    <div class="content">
+        <div class="title m-b-md">
+            Register
+        </div>
+        <div>
+            <input type="text" name="user" class="user input" placeholder="User Name">
+        </div>
+        <div>
+            <input type="password" name="password" class="password input" placeholder="Password">
+        </div>
+        <div>
+            <input type="password" class="password input" placeholder="Confirm password">
+        </div>
+        <div class="submit_div">
+            <button>Submit</button>
+        </div>
+    </div>
+</div>
+</body>
+</html>
+<style>
+
+</style>
